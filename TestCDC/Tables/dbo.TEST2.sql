35985920448ce7249d44a3ed81066ea10c790180
hem.haria <hem.haria@justgiving.com>
2012-10-11 10:03:47 +0100

    Added new col test4

--- TestCDC/Tables/dbo.TEST2.sql
+++ TestCDC/Tables/dbo.TEST2.sql
@@ -1,6 +1,7 @@
 CREATE TABLE [dbo].[TEST2]
 (
 [ID] [int] NOT NULL IDENTITY(1, 1),
-[TEST3] [nvarchar] (100) COLLATE Latin1_General_CI_AS NULL
+[TEST3] [nvarchar] (100) COLLATE Latin1_General_CI_AS NULL,
+[TestCol4] [int] NULL
 ) ON [PRIMARY]
 GO
